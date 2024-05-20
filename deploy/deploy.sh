cb7abf57ec9041cdada9389af0f42efece5c9707
rushikeshmj <rushikeshja@cybage.com>
2024-05-20 15:01:16 +0530

    Create deploy.sh

---
+++ deploy/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 781365762844.dkr.ecr.us-east-1.amazonaws.com
+
+docker pull 781365762844.dkr.ecr.us-east-1.amazonaws.com/githubaction-poc:latest
