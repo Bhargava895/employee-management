dae0e6e399e31ec826e73a09998eef3652b3f64c
Bhargava895 <37539869+Bhargava895@users.noreply.github.com>
2024-05-24 19:44:20 +0530

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 781365762844.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 905418319927.dkr.ecr.us-east-1.amazonaws.com
 
-#docker rm -f 781365762844.dkr.ecr.us-east-1.amazonaws.com/githubaction-poc:latest
+#docker rm -f 905418319927.dkr.ecr.us-east-1.amazonaws.com/githubaction-poc:latest
 
-docker rmi -f 781365762844.dkr.ecr.us-east-1.amazonaws.com/githubaction-poc:latest
+docker rmi -f 905418319927.dkr.ecr.us-east-1.amazonaws.com/githubaction-poc:latest
 
-docker pull 781365762844.dkr.ecr.us-east-1.amazonaws.com/githubaction-poc:latest
+docker pull 905418319927.dkr.ecr.us-east-1.amazonaws.com/githubaction-poc:latest
 
-#docker run 781365762844.dkr.ecr.us-east-1.amazonaws.com/githubaction-poc:latest
+#docker run 905418319927.dkr.ecr.us-east-1.amazonaws.com/githubaction-poc:latest
